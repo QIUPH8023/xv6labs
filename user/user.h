bde5832f60a77ce209949bb6d374ad8422a5e1af
QIUPH <qiuph@debian.myguest.virtualbox.org>
2024-08-16 10:57:46 +0800

    complete lab10

--- user/user.h
+++ user/user.h
@@ -24,6 +24,9 @@ char* sbrk(int);
 int sleep(int);
 int uptime(void);
 
+void* mmap(void *addr, int len, int prot, int flags, int fd, uint offset);
+int munmap(void *addr, int len);
+
 // ulib.c
 int stat(const char*, struct stat*);
 char* strcpy(char*, const char*);
